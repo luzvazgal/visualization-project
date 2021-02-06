91232341a8eafd805356ed6f25179df95ed49dfb
Luz Vazquez Galindo <luzvazgal@MacBook-Air-de-Luz.local>
2021-02-06 14:34:05 -0600

    comparing geoJSON with selected country info

--- src/js/in_out_map.js
+++ src/js/in_out_map.js
@@ -1,3 +1,4 @@
+//Map definition
 var in_out_map = L.map('in_out_map').setView([0,0],.5)
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,46 +12,82 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 
 
+//When user changes selection, it will bring selected country
+d3.selectAll("#Country_select").on('change', function(){
 
+    console.log("entro")
+
+    //Setting country data
+    setCountryData();
+
+    //Getting inbound and outbond countries as arrays
+    let in_countries_list = Object.keys(inbound_countries.Top_Markets);
+    let out_countries_list = Object.keys(outbound_countries.Top_Destinations);
+
+    getGeoJsonData(in_countries_list, out_countries_list);
+    console.log(in_countries_list);
+    console.log(out_countries_list);
+ 
+});
 
 /**
- * Draws country and its inbound and outbound tourism countries 
+ * Getting geoJSONData according to user's selection
  */
-function selectCountry(){
-
-    //Getting user's selection
-    let selected_country = d3.select("#Country_select").node().value; 
-
-    //Getting country's selected related data
-    let selected_country_data = data.filter( 
-        record => record.name == selected_country
-    )
-    
-    let inbound_countries, outbound_countries;
-
-    selected_country_data.forEach(record =>{
-        inbound_countries = Object.keys(record.Inbound.Top_Markets);
-        outbound_countries = Object.keys(record.Outbound.Top_Destinations);
-    }
-    )
-
-    console.log(inbound_countries);
-    console.log(outbound_countries);
-
+function getGeoJsonData(in_countries_list, out_countries_list){
     //Reading geojson file containing all countries coordinates
-    d3.json(geojson_file).then(record=>{
+    d3.json(geojson_file).then(geoJsondata=>{
+    
+        //console.log("Features "+geoJsondata.features);
+ 
+        //Variables Definition to assigned geoJSON records for selected country, and its corresponding inbound and outbound countries.
+        let geoJSON_selected_country;
+        let geoJSON_inbound_countries =[];
+        let geoJSON_outbound_countries = [];
+ 
+        //Getting geoJSON records from selected country, inbound and outbound countries.
+        let geoJson_selected= geoJsondata.features.map(record=>{
+            
+            //Current geoJSON country
+            geoJson_country = record.properties.name;
         
-        //record.filter(function checkCountry()
-        //{
-               console.log(record);}
+            //if current geoJson country equals selected country, assign geoJSON object to geoJSON_selected_country
+            if(geoJson_country == selected_country){
+                geoJSON_selected_country = record;
+            }
+            
+            //Iterating on inbound countries list 
+            in_countries_list.forEach(country=>{
+                //Replacing '_' character by space ' '
+                country = country.replace('_', ' ');
 
-    //)}
-    )
-    .catch(function(error) {
-        console.log(error);
-    });
+                //if current geoJson country equals inbound country, assign that geoJSON object to geoJSON_inbound_country
+                if(geoJson_country == country){
+                    geoJSON_inbound_countries.push(record);
+                }
+            })
 
+            //Iterating on outbound countries list 
+            out_countries_list.forEach(country=>{
+                 //Replacing '_' character by space ' '
+                 country = country.replace('_', ' ');
 
+                //if current geoJson country equals inbound country, assign that geoJSON object to geoJSON_outbound_country
+                if(geoJson_country == country){
+                    geoJSON_outbound_countries.push(record);
+                }
+            })
+        })
+
+        console.log("geoJSON selected");
+        console.log(geoJSON_selected_country);
+        console.log(geoJSON_inbound_countries);
+        console.log(geoJSON_outbound_countries);
+     
+ }
+ )
+ .catch(function(error) {
+     console.log(error);
+ })
 }
 
 function getCoordinates()
